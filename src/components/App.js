b093d1806035ed3555fa899227b081f9e6df4b49
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-01-31 01:16:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useMemo, useState } from "react";
+// import "./../styles/App.css";
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const data = await res.json();
+      setData(data);
+      console.log(data);
+    };
+    fetchData();
+  }, []);
+
+  const MemoizedData = useMemo(() => data, [data]);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Cached API</h1>
+      <ul>
+        {data &&
+          MemoizedData.map((post, index) => (
+            <li key={index}>
+              <h4>{post.title}</h4>
+              <p>{post.body}</p>
+            </li>
+          ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
